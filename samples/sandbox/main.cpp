279a198c0f4c3bad7dc6b5e346cd13481e0d30c9
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-01-08 21:30:03 +0100

    changes

--- samples/sandbox/main.cpp
+++ samples/sandbox/main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <vdtgraphics/opengl/opengl.h>
+#include <GLFW/glfw3.h>
 #include <vdtgraphics/graphics.h>
 
 using namespace std;
 using namespace graphics;
 
-int main()
+int main(void)
 {
-	
+    GLFWwindow* window;
 
-	return getchar();
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
