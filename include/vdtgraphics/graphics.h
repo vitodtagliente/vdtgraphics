d51f7897c123e7f7751746fa76fa1a292e95fb95
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-06-18 22:36:09 +0200

    changes

--- include/vdtgraphics/graphics.h
+++ include/vdtgraphics/graphics.h
@@ -1,3 +1,17 @@
 /// Copyright (c) Vito Domenico Tagliente
 #pragma once
 
+#include "blend_state.h"
+#include "buffer.h"
+#include "color.h"
+#include "index_buffer.h"
+#include "polygon_batch.h"
+#include "renderable.h"
+#include "renderer.h"
+#include "shader.h"
+#include "shader_library.h"
+#include "shader_program.h"
+#include "texture.h"
+#include "texture_coords.h"
+#include "texture_rect.h"
+#include "vertex_buffer.h"
