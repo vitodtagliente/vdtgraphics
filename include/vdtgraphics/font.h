95c6be4cdc57898f7ea6b05d3aee5c4cf4559aba
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2023-01-30 22:50:24 +0100

    font data

--- include/vdtgraphics/font.h
+++ include/vdtgraphics/font.h
@@ -2,34 +2,42 @@
 #pragma once
 
 #include <filesystem>
+#include <map>
 #include <string>
 
 #include <vdtmath/vector2.h>
-#include "texture_coords.h"
+
+#include "texture.h"
 
 namespace graphics
 {
+	struct Glyph final
+	{
+		// offset to advance to next glyph
+		unsigned int advance;
+		// offset from baseline to left/top of glyph
+		math::vec2 bearing;
+		// the size of the glyph
+		math::vec2 size;
+		std::shared_ptr<Texture> texture;
+	};
+
 	struct Font final
 	{
 		Font();
-		Font(const std::shared_ptr<unsigned char>& data, const std::shared_ptr<void>& glyphInfo, const std::filesystem::path& path);
+		Font(const std::map<char, Glyph>& data, const std::filesystem::path& path);
 		Font(const Font& other);
 		~Font();
 
 		static Font load(const std::filesystem::path& filename);
+
+		inline bool isValid() const { return !data.empty(); }
 
 		Font& operator= (const Font& other);
 		bool operator== (const Font& other) const;
 		bool operator!= (const Font& other) const;
 
 		std::filesystem::path path;
-		std::shared_ptr<unsigned char> data;
-		std::shared_ptr<void> glyphInfo;
-
-		static constexpr int width = 1024;
-		static constexpr int height = 1024;
-
-		static constexpr uint32_t oversampleX = 2;
-		static constexpr uint32_t oversampleY = 2;
+		std::map<char, Glyph> data;
 	};
 }
--- source/font.cpp
+++ source/font.cpp
@@ -1,29 +1,36 @@
 #include <vdtgraphics/font.h>
 
-#include <fstream>
+#include <glad/glad.h>
 
-#define STB_TRUETYPE_IMPLEMENTATION 
-#include <vdtgraphics/stb_truetype.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H  
 
 namespace graphics
 {
+	namespace
+	{
+		struct Context
+		{
+			FT_Library data;
+			bool initialized = false;
+			bool ready = false;
+		} context{};
+	}
+
 	Font::Font()
 		: data()
-		, glyphInfo()
 		, path()
 	{
 	}
 
-	Font::Font(const std::shared_ptr<unsigned char>& data, const std::shared_ptr<void>& glyphInfo, const std::filesystem::path& path)
+	Font::Font(const std::map<char, Glyph>& data, const std::filesystem::path& path)
 		: data(data)
-		, glyphInfo(glyphInfo)
 		, path(path)
 	{
 	}
 
 	Font::Font(const Font& other)
 		: data(other.data)
-		, glyphInfo(other.glyphInfo)
 		, path(other.path)
 	{
 	}
@@ -34,55 +41,63 @@ namespace graphics
 
 	Font Font::load(const std::filesystem::path& path)
 	{
-		std::ifstream file(path, std::ios::binary | std::ios::ate);
-		if (!file.is_open())
-			return Font();
+		if (!context.initialized)
+		{
+			context.ready = FT_Init_FreeType(&context.data) == 0;
+		}
 
-		const auto size = file.tellg();
-		file.seekg(0, std::ios::beg); 
-		auto bytes = std::vector<unsigned char>(size);
-		file.read(reinterpret_cast<char*>(&bytes[0]), size);
-		file.close();
+		if (!context.ready) return Font({}, path);
 
-		unsigned char* data = new unsigned char[width * height]();
-		
-		static const uint32_t firstChar = ' ';
-		static const uint32_t charCount = '~' - ' ';
-		static const int fontSize = 40;
-		stbtt_packedchar* charInfo = new stbtt_packedchar[charCount]();
+		FT_Face face;
+		if (FT_New_Face(context.data, path.string().c_str(), 0, &face) != 0)
+		{
+			return Font({}, path);
+		}
 
-		stbtt_pack_context context;
-		if (!stbtt_PackBegin(&context, data, width, height, 0, 1, nullptr))
-			return Font();
+		FT_Set_Pixel_Sizes(face, 0, 48);
+		std::map<char, Glyph> data;
+		for (unsigned char c = 0; c < 128; c++)
+		{
+			// load character glyph 
+			if (FT_Load_Char(face, c, FT_LOAD_RENDER) != 0)
+			{
+				continue;
+			}
 
-		stbtt_PackSetOversampling(&context, oversampleX, oversampleY);
-		if (!stbtt_PackFontRange(&context, &bytes[0], 0, fontSize, firstChar, charCount, charInfo))
-			return Font();
+			Texture::Options options;
+			options.wrapS = GL_CLAMP_TO_EDGE;
+			options.wrapT = GL_CLAMP_TO_EDGE;
+			options.filterMin = GL_LINEAR;
+			options.filterMax = GL_LINEAR;
+			TexturePtr texture = std::make_shared<Texture>(face->glyph->bitmap.buffer, face->glyph->bitmap.width, face->glyph->bitmap.rows, 3);
+			
+			Glyph glyph = {
+				static_cast<unsigned int>(face->glyph->advance.x),
+				math::vec2(static_cast<float>(face->glyph->bitmap_left), static_cast<float>(face->glyph->bitmap_top)),
+				math::vec2(static_cast<float>(face->glyph->bitmap.width), static_cast<float>(face->glyph->bitmap.rows)),
+				std::move(texture)
+			};
+			data.insert(std::pair<char, Glyph>(c, glyph));
+		}
 
-		stbtt_PackEnd(&context);
+		FT_Done_Face(face);
 
-		return Font(std::shared_ptr<unsigned char>(data), std::shared_ptr<void>(charInfo), path);
+		return Font(data, path);
 	}
 
 	Font& Font::operator=(const Font& other)
 	{
-		data = other.data;
-		glyphInfo = other.glyphInfo;
 		path = other.path;
 		return *this;
 	}
 
 	bool Font::operator==(const Font& other) const
 	{
-		return data == other.data
-			&& glyphInfo == other.glyphInfo
-			&& path == other.path;
+		return path == other.path;
 	}
 
 	bool Font::operator!=(const Font& other) const
 	{
-		return data != other.data
-			|| glyphInfo != other.glyphInfo
-			|| path != other.path;
+		return path != other.path;
 	}
 }
